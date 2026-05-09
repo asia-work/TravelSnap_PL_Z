34e3a9b5823b32f47e2882187a106e79d9fa71df
Joanna <burskaasiawork@gmail.com>
2026-05-09 14:45:56 +0200

    Poprawienie switcha i danie alternatywnego rozwiązania w komentarzu dot. ratingu

--- components/RatingStars.tsx
+++ components/RatingStars.tsx
@@ -12,5 +12,9 @@ export default function RatingStars({ stars }: { stars: number }) {
       return <Text>★★★★☆</Text>;
     case 5:
       return <Text>★★★★★</Text>;
+    default:
+      return <Text>błędna ocena</Text>;
   }
+  // // Opcjonalne inne podejście
+  // return <Text>{"★".repeat(stars) + "☆".repeat(5 - stars)}</Text>;
 }
